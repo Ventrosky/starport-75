4ef3fa641fc43cb55d48b72ce731c9d29bfdd279
Ventrosky <ventrosky@gmail.com>
2021-08-08 10:16:49 +0200

    🚧 trying out different locale

--- lambda/languageStrings.js
+++ lambda/languageStrings.js
@@ -10,5 +10,17 @@ module.exports = {
             ERROR_MSG: 'Sorry, I had trouble doing what you asked. ' +
                 'Please try again.'
         }
+    },
+    it: {
+        translation: {
+            WELCOME_MSG: 'Benvenuto a Star Port 75 Travel. Come posso aiutarti?',
+            HELLO_MSG: 'Buona giornata stellare!',
+            HELP_MSG: 'Puoi salutarmi! Come posso aiutare?',
+            GOODBYE_MSG: 'Arrivederci!',
+            REFLECTOR_MSG: 'Hai appena triggerato {{intentName}}',
+            FALLBACK_MSG: 'Mi dispiace, non lo so. Per favore riprova.',
+            ERROR_MSG: 'Scusa, ho avuto problemi a fare quello che mi hai chiesto. ' +
+                'Per favore riprova.'
+        }
     }
 }
